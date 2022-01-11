c6c7364ad8fafaba7b8961f181e1490c7fdc29cf
casualbill <hotwindzb@gmail.com>
2022-01-11 16:30:06 +0800

    Add title background color and homepage bg layer

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,6 @@
 <template>
   <view class="index-page">
+    <view class="background-layer" />
     <swiper class="swiper" indicator-dots autoplay>
       <swiper-item>
         <image class="swiper-image" src="/static/img-banner1.jpg"></image>
@@ -36,6 +37,14 @@ export default {
   justify-content: center;
   padding-bottom: 80rpx;
   background-color: #E5E5E5;
+}
+
+.background-layer {
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  height: 336rpx;
+  background-color: #105B63;
 }
 
 .swiper {
