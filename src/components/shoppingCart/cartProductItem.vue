e5fe1c2f033848e65701361d2573383d0063f4d9
casualbill <hotwindzb@gmail.com>
2022-01-14 18:29:47 +0800

    Pick up shopping cart product item as a component

---
+++ src/components/shoppingCart/cartProductItem.vue
@@ -0,0 +1,111 @@
+<template>
+  <view class="cart-item">
+    <view class="cart-checkbox" />
+    <image class="product-image" :src="props.image" />
+    <view class="product-content-block">
+      <view class="product-content-row">
+        <text class="product-name">{{ props.name }}</text>
+        <image class="remove-icon" src="/static/ico-trash.svg" />  
+      </view>
+      <view class="product-content-row">
+        <view class="product-amount-block">
+          <button class="amount-single-box amount-adjust-button">-</button>
+          <text class="amount-single-box amount-number">1</text>
+          <button class="amount-single-box amount-adjust-button">+</button>
+        </view>
+        <text class="price-text">￥{{ props.price }}</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+	name: String,
+  image: String,
+  price: String,
+});
+</script>
+
+<style lang="scss" scoped>
+.cart-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 160rpx;
+  margin: 0 32rpx;
+  padding: 16rpx;
+  border-bottom: 2rpx solid rgba(0, 0, 0, 0.1);
+  
+  .cart-checkbox {
+    width: 48rpx;
+    height: 48rpx;
+    border: 4rpx solid #105B63;
+    border-radius: 18rpx;
+  }
+  
+  .product-image {
+    display: block;
+    width: 160rpx;
+    height: 160rpx;
+  }
+  .product-content-block {
+    display: flex;
+    flex-direction: column;
+    width: 400rpx;
+    height: 160rpx;
+    
+    .product-content-row {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      flex: 1;
+      width: 400rpx;
+    }
+    
+    .product-name {
+      height: 60rpx;
+      line-height: 60rpx;
+      margin-top: 10rpx;
+      font-size: 28rpx;
+    }
+    
+    .remove-icon {
+      width: 32rpx;
+      height: 32rpx;
+      margin: -10rpx -10rpx 0 0;
+      padding: 10rpx;
+    }
+
+    .product-amount-block {
+      display: flex;
+      flex-direction: row;
+
+      .amount-single-box {
+        width: 48rpx;
+        height: 48rpx;
+        line-height: 48rpx;
+        margin: 0 8rpx 0 0;
+        padding: 0;
+        border-radius: 18rpx;
+        text-align: center;
+        
+        &.amount-adjust-button {
+          background-color: #105B63;
+          color: #FFFFFF;
+          font-size: 32rpx;
+        }
+        &.amount-number {
+          background-color: #EBEAEF;
+          color: #000000;
+          font-size: 20rpx;
+        }  
+      }      
+    }
+    
+    .price-text {
+      font-size: 32rpx;
+    }
+  }
+}
+</style>
--- src/pages/shoppingCart/shoppingCart.vue
+++ src/pages/shoppingCart/shoppingCart.vue
@@ -1,126 +1,13 @@
 <template>
 	<view>
-    <view class="cart-item">
-      <view class="cart-checkbox" />
-      <image class="product-image" src="/static/product/img-product1.png" />
-      <view class="product-content-block">
-        <view class="product-content-row">
-          <text class="product-name">青椒</text>
-          <image class="remove-icon" src="/static/ico-trash.svg" />  
-        </view>
-        <view class="product-content-row">
-          <view class="product-amount-block">
-            <button class="amount-single-box amount-adjust-button">-</button>
-            <text class="amount-single-box amount-number">1</text>
-            <button class="amount-single-box amount-adjust-button">+</button>
-          </view>
-          <text class="price-text">￥18</text>
-        </view>
-      </view>
-    </view>
-    <view class="cart-item">
-      <view class="cart-checkbox" />
-      <image class="product-image" src="/static/product/img-product2.png" />
-      <view class="product-content-block">
-        <view class="product-content-row">
-          <text class="product-name">鸡胸肉</text>
-          <image class="remove-icon" src="/static/ico-trash.svg" />  
-        </view>
-        <view class="product-content-row">
-          <view class="product-amount-block">
-            <button class="amount-single-box amount-adjust-button">-</button>
-            <text class="amount-single-box amount-number">3</text>
-            <button class="amount-single-box amount-adjust-button">+</button>
-          </view>
-          <text class="price-text">￥688.88</text>
-        </view>
-      </view>
-    </view>
+    <cart-product-item name="青椒" image="/static/product/img-product1.png" price="18" />
+    <cart-product-item name="鸡胸肉" image="/static/product/img-product2.png" price="688.88" />
   </view>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import cartProductItem from '@/components/shoppingCart/cartProductItem.vue';
 </script>
 
 <style lang="scss" scoped>
-.cart-item {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: 160rpx;
-  margin: 0 32rpx;
-  padding: 16rpx;
-  border-bottom: 2rpx solid rgba(0, 0, 0, 0.1);
-  
-  .cart-checkbox {
-    width: 48rpx;
-    height: 48rpx;
-    border: 4rpx solid #105B63;
-    border-radius: 18rpx;
-  }
-  
-  .product-image {
-    display: block;
-    width: 160rpx;
-    height: 160rpx;
-  }
-  .product-content-block {
-    display: flex;
-    flex-direction: column;
-    width: 400rpx;
-    height: 160rpx;
-    
-    .product-content-row {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      flex: 1;
-      width: 400rpx;
-    }
-    
-    .product-name {
-      height: 60rpx;
-      line-height: 60rpx;
-      margin-top: 10rpx;
-      font-size: 28rpx;
-    }
-    
-    .remove-icon {
-      width: 32rpx;
-      height: 32rpx;
-      margin: -10rpx -10rpx 0 0;
-      padding: 10rpx;
-    }
-
-    .product-amount-block {
-      display: flex;
-      flex-direction: row;
-
-      .amount-single-box {
-        width: 48rpx;
-        height: 48rpx;
-        line-height: 48rpx;
-        margin: 0 8rpx 0 0;
-        padding: 0;
-        border-radius: 18rpx;
-        text-align: center;
-        
-        &.amount-adjust-button {
-          background-color: #105B63;
-          color: #FFFFFF;
-          font-size: 32rpx;
-        }
-        &.amount-number {
-          background-color: #EBEAEF;
-          color: #000000;
-          font-size: 20rpx;
-        }  
-      }      
-    }
-    
-    .price-text {
-      font-size: 32rpx;
-    }
-  }
-}
 </style>
