51f9da2bf84b4397aa479282213a6b818a4d899a
casualbill <hotwindzb@gmail.com>
2022-01-18 16:16:09 +0800

    Add my profile page with profile item component

---
+++ src/components/myProfile/profileItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <view class="profile-item">
+    <text class="item-content-text">{{ props.itemName }}</text>
+    <image class="profile-icon" src="/static/ico-entry.svg" />
+  </view>
+</template>
+
+<script lang="ts" setup>
+interface IProps {
+  itemName: String;
+}
+const props = defineProps<IProps>();
+</script>
+
+<style lang="scss" scoped>
+.profile-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 686rpx;
+  height: 64rpx;
+  margin-top: 16rpx;
+  padding: 16rpx;
+  border-radius: 24rpx;
+  background: #ffffff;
+  box-shadow: 0 20rpx 40rpx rgba(0, 0, 0, 0.08);
+
+  .item-content-text {
+    margin-left: 16rpx;
+    font-size: 24rpx;
+    font-weight: 600;
+  }
+
+  .profile-icon {
+    width: 64rpx;
+    height: 64rpx;
+  }
+}
+</style>
--- src/pages/myProfile/myProfile.vue
+++ src/pages/myProfile/myProfile.vue
@@ -1,7 +1,56 @@
 <template>
-  <view>MyProfile</view>
+  <view class="profile-page">
+    <view class="user-info">
+      <image class="user-avatar" src="/static/ico-avatar.png" />
+      <text class="user-name-text">John Doe</text>
+      <text class="user-mobile-text">188******99</text>
+    </view>
+    <view class="profile-list">
+      <profile-item itemName="订单" />
+      <profile-item itemName="账户设置" />
+      <profile-item itemName="纪念品" />
+      <profile-item itemName="优惠券" />
+      <profile-item itemName="分享" />
+    </view>
+  </view>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts" setup>
+import profileItem from '@/components/myProfile/profileItem.vue';
+</script>
 
-<style></style>
+<style lang="scss" scoped>
+.profile-page {
+  .user-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 48rpx;
+
+    .user-avatar {
+      width: 144rpx;
+      height: 144rpx;
+      margin-top: 8rpx;
+    }
+    .user-name-text {
+      height: 72rpx;
+      line-height: 72rpx;
+      margin-top: 16rpx;
+      font-size: 48rpx;
+      font-weight: 600;
+    }
+    .user-mobile-text {
+      height: 32rpx;
+      line-height: 32rpx;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 24rpx;
+    }
+  }
+
+  .profile-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
